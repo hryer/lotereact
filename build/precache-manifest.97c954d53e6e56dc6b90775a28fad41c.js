aae914ece53a3816f4186950a6927e04cfc85baf
MaliciousHex <harryermawan@gmail.com>
2018-10-14 17:12:22 +0700

    setup

---
+++ build/precache-manifest.97c954d53e6e56dc6b90775a28fad41c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0493130bd50378404225",
+    "url": "/static/js/main.0493130b.chunk.js"
+  },
+  {
+    "revision": "2f4ac4a68bf5829bd583",
+    "url": "/static/js/1.2f4ac4a6.chunk.js"
+  },
+  {
+    "revision": "0493130bd50378404225",
+    "url": "/static/css/main.34246f7d.chunk.css"
+  },
+  {
+    "revision": "99e3270f93d042d46999c886b313011d",
+    "url": "/index.html"
+  }
+];
