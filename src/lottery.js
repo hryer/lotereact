9997114b3264282e1837eb281de1ef15063f0261
MaliciousHex <harryermawan@gmail.com>
2018-10-26 14:00:23 +0700

    update

---
+++ src/lottery.js
@@ -0,0 +1,67 @@
+import web3 from './web3';
+
+const address = '0x97a57d3E30ED73ca829cF041bB4d9717c9A12BF9';
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: 'cek',
+    outputs: [{ name: '', type: 'uint256' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'manager',
+    outputs: [{ name: '', type: 'address' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: 'pickWinner',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'getPlayers',
+    outputs: [{ name: '', type: 'address[]' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: 'enter',
+    outputs: [],
+    payable: true,
+    stateMutability: 'payable',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [{ name: '', type: 'uint256' }],
+    name: 'players',
+    outputs: [{ name: '', type: 'address' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'constructor'
+  }
+];
+
+export default new web3.eth.Contract(abi, address);
